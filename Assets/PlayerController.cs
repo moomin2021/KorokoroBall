96d900711a8c1c169faa26c5a71696e7dc59132e
tamura_fumiya <k021g1134@g.neec.ac.jp>
2022-04-26 13:57:20 +0900

    プレイヤーを右と後ろに動けるように追記

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -17,11 +17,26 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        // 上キーを押したら前に力を加える
         if (Input.GetKey(KeyCode.UpArrow))
         {
             rigidbody.AddForce(new Vector3(0, 0, 1) * power);
         }
 
+        // 下キーを押したら後ろに力を加える
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rigidbody.AddForce(new Vector3(0, 0, -1) * power);
+        }
+
+        // 右キーを押したら右に力を加える
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rigidbody.AddForce(new Vector3(1, 0, 0) * power);
+        }
+
+        // 左キーを押したら左に力を加える
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             rigidbody.AddForce(new Vector3(-1, 0, 0) * power);
